52e1729b89b6adcc965e0875ca0928eb306b9764
Alexander <alexander88echols@gmail.com>
2022-07-27 00:11:54 +0000

    I added some text and made a output to the terminal

---
+++ helloworld.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Alexander Echols                      
+# Date of latest revision: 26 July 2022      
+# Purpose: Print a string to the terminal
+
+
+# Main
+echo "I am working decently"
+# End
