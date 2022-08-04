fa1c7a0dbe404148b3296c29dbfd7cbea804c86c
R00sterGuy <alexander88echols@gmail.com>
2022-08-04 23:43:07 +0000

    updated

--- 09Lab.sh
+++ 09Lab.sh
@@ -1,5 +1,10 @@
 # Get-EventLog -LogName System -After (Get-Date).AddHours(-24) | Out-File -FilePath C:\Users\knott\OneDrive\Desktop\last_24.txt
+# will create a file on my desktop comprised of the events of the System event log that have occured over the last 24 hrs
 # Get-EventLog -LogName System -EntryType Error | Out-File -FilePath C:\Users\knott\OneDrive\Desktop\error.txt
+# will create a file on my desktop comprised of the errors of the System event log
 # Get-EventLog -LogName System -InstanceId 16 
+# will out put all system logs that have the id number 16
 # Get-EventLog -LogName System -Newest 20
+# outputs the newest events from the system log
 # Get-EventLog -LogName System -Newest 500 | Select-Object -Property source
+# outputs the the 500 most recent entries in the system event log from all sources
