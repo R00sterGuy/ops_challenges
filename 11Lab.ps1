69ca6938495d75f7a54fd4355757b619999a1ed0
R00sterGuy <alexander88echols@gmail.com>
2022-08-09 00:28:21 +0000

    my powershell script

--- 11Lab.ps1
+++ 11Lab.ps1
@@ -1,7 +1,7 @@
 # will enable file and printer sharing
 Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
 # will allow ICMP traffic
-netsh advfirwall firewall add rule name="Allow incoming ping requests 1pv4" dir=in action-allow protocol-icmpv4
+netsh advfirwall firewall add rule name="Allow incoming ping requests 1pv4" dir=in action=allow protocol=icmpv4
 # the next two will add and enable the use of remote management with the use of Network Level Authintication
 Reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 Set-itemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -nAME "UserAuthentication" -Value 1
